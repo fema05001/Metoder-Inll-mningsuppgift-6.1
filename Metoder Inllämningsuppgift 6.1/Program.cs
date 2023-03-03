da7093491f06b907c27b8edec9fae2e5a30fd2e6
Elev <Elev@TEB125102>
2023-03-03 13:57:59 +0100

    Add project files.

---
+++ Metoder Inllämningsuppgift 6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace inlämningsuppgift
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Störst(5, 2, 1)); 
+        }
+        static int Störst(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
